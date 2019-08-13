c843f9539a5eb4bcb575b884b20785b6dd915828
hlarson <aa-badalov1@mail.ru>
2019-08-13 20:39:25 +0300

    print_map

--- srcs/print_map.c
+++ srcs/print_map.c
@@ -6,13 +6,40 @@
 /*   By: hlarson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 19:04:56 by hlarson           #+#    #+#             */
-/*   Updated: 2019/08/13 20:14:49 by hlarson          ###   ########.fr       */
+/*   Updated: 2019/08/13 20:39:07 by hlarson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-void	print_map(t_organiser *organizer)
+static void	help_print_connections(t_organiser *organizer)
+{
+	t_graph *graph;
+	size_t	i;
+	size_t	j;
+	t_list	*adjacency;
+	t_graph	*tmp;
+
+	graph = organizer->graph;
+	i = 0;
+	while (i < organizer->size)
+	{
+		adjacency = graph[i].adjacency;
+		while (adjacency)
+		{
+			tmp = (t_graph *)(adjacency->content);
+			j = 0;
+			while (tmp->x != graph[i + j].x && (i + j) < organizer->size)
+				j++;
+			if (j > 0 && (j + i) < organizer->size)
+				ft_printf("%s-%s\n", graph[i].name, tmp->name);
+			adjacency = adjacency->next;
+		}
+		i++;
+	}
+}
+
+void		print_map(t_organiser *organizer)
 {
 	t_graph *graph;
 	size_t	i;
@@ -25,4 +52,5 @@ void	print_map(t_organiser *organizer)
 		ft_printf("%s %d %d\n", graph[i].name, graph[i].x, graph[i].y);
 		i++;
 	}
+	help_print_connections(organizer);
 }
